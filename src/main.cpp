7e44886da5c7f6fb855040446ac9db330299660e
rb <102971493+rb-uf@users.noreply.github.com>
2022-04-17 10:45:01 -0400

    Change "anatree" flag to "--anatree"

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
     Anatree anatree(dict);
 
 	string input;
-    if (argc > 1 && string(argv[1]) == "anatree")
+    if (argc > 1 && string(argv[1]) == "--anatree")
         while (getline(cin, input))
     		anatree.printAnagramPhrases(input);
     else
